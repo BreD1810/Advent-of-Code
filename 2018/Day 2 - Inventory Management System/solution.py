6739d0fc11fe8c375d2fdab84dcaad65c5011b37
BreD1810 <32489229+BreD1810@users.noreply.github.com>
2018-12-24 21:28:47 +0000

    Added solution for 2018 day 2 part 2

--- 2018/Day 2 - Inventory Management System/solution.py
+++ 2018/Day 2 - Inventory Management System/solution.py
@@ -3,7 +3,6 @@ from string import ascii_lowercase
 file = open("input.txt", "r")
 box_ids = file.readlines()
 
-
 def get_checksum(ids):
     double = 0
     triple = 0
@@ -27,4 +26,14 @@ def contains_triple_letter(id):
     return False
 
 
-print(get_checksum(box_ids))
+def get_common_letters_of_prototypes(ids):
+    for id in ids:
+        for other_id in ids:
+            if id != other_id:
+                matching_characters = [id[index] for index in range(0, len(id)) if id[index] == other_id[index]]
+                if len(matching_characters) == len(id) - 1: return "".join(matching_characters)
+    return ""
+
+
+print("Part 1: " + str(get_checksum(box_ids)))
+print("Part 2: " + get_common_letters_of_prototypes(box_ids))
